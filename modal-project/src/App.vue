ed355172482ba4ad0e1f3971028a869790d2a216
Steffan Mouton <moutonsteffan@gmail.com>
2022-06-21 22:32:02 -0500

    Split components, pass data as prop

--- modal-project/src/App.vue
+++ modal-project/src/App.vue
@@ -1,26 +1,33 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ title }}</h1>
+    <Modal :header="header" :text="text" theme="sale"/>
 </template>
 
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from './components/Modal.vue'
+
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    components: { Modal },
+    data() {
+        return {
+            title: 'My First Vue App!',
+            header: 'Sign up for the Giveaway!',
+            text: 'Get swag at half price'
+        }
+    },
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
